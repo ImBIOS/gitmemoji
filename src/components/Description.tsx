1778fc45150a797c9aab32cc61537bebe5ba34b0
Nikhil <84772872+tymnec@users.noreply.github.com>
2023-04-06 19:39:35 +0530

    :white_check_mark: fix: removed non-useful files

--- src/components/Description.tsx
+++
@@ -1,8 +0,0 @@
-// const Description = () => {
-//   return (
-//     <div className="text-3xl">
-//     </div>
-//   );
-// };
-
-// export default Description;
--- src/components/ExternalLink.tsx
+++
@@ -1,9 +0,0 @@
-// const ExternalLink = () => {
-//   return (
-//     <a className="text-red-500 no-underline">
-//       {/* {children} */}
-//     </a>
-//   );
-// };
-
-// export default ExternalLink;
